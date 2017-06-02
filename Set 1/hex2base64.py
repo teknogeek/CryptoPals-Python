7704d48d68ae1de856aa10e4193484b536555297
Joel Margolis <jmargolis97@gmail.com>
2017-06-02 11:33:07 -0400

    Add hex2base64

---
+++ Set 1/hex2base64.py
@@ -0,0 +1,15 @@
+from binascii import unhexlify
+from base64 import b64encode
+from sys import argv
+
+def main():
+    if len(argv) == 2:
+        hexData = unhexlify(argv[1])
+        b64Data = b64encode(hexData).decode('utf-8')
+        
+        print(b64Data)
+    else:
+        print('Usage: python hex2base64.py [hex string]')
+
+if __name__ == '__main__':
+    main()
